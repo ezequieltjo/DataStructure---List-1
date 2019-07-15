c0a49a43ad52ef4feae6cf971ddf9dc6e018b4f6
Ezequiel Teotônio Jó <41303804+ezequieltjo@users.noreply.github.com>
2019-07-15 13:56:07 -0300

    Add files via upload

---
+++ teste.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct stack{
+    char wagons[26];
+
+
+} stack;
+
+void push(struct stack*s ){
+
+}
+
+void pop(struct stack*s){
+
+}
+
+int main (){
+
+    int total=0;
+
+    while(scanf("%d", total)>=1){
+        char input[total];
+        char output[total];
+
+        scanf("%[^\n]", input);
+        scanf("%[^\n]", output);
+
+
+
+    }
+    return 0;
+}
